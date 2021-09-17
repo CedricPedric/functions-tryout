39d03d9e068247165fdfcfc17bd9abe225070f9f
CedricPedric <99059069@mydavinci.nl>
2021-09-17 11:32:27 +0200

    commit multiplier-x.py

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@ def greet():
     print("Hello from function town")
 
 greet()
+
+# def showWelcome(welcomeTo='town'):
+#     print('Welcome to function'+ welcomeTo)
+
+# showWelcome()
---
+++ multiplier-x.py
@@ -0,0 +1,9 @@
+
+def keer(x):
+    for a in range(1,11):
+       print( x * a )
+
+
+getal = int(input("Vul een getal in: "))
+
+keer(getal)
