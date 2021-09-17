7115564946221b8ec0d33fbf9ee604df664a9cb4
CedricPedric <99059069@mydavinci.nl>
2021-09-17 17:45:19 +0200

    programma2,py eerste commit

---
+++ programma2.py
@@ -0,0 +1,26 @@
+def addition(number1, number2):
+    return  number1 + number2
+
+def subtraction(number1, number2):
+    return number1 - number2
+
+def multiplication(number1, number2):
+    return number1 * number2
+
+def division(number1, number2):
+    return number1 / number2
+
+def increment(number):
+    return number + 1
+
+def decrement(number):
+    return number - 1
+
+
+print(addition(10, 12))
+print(subtraction(58, 34))
+print(multiplication(6,7))
+print(division(144,12))
+print(increment(12))
+print(decrement(34))
+
